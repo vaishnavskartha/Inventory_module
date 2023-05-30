8015fb5406876b0964e6079badb8a9fd866dc453
vaishnavskartha <vaishnavskartha@gmail.com>
2023-05-30 22:43:42 +0530

    new

--- backend/models/Inventory/itemsGroupModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const itemsGroupSchema = mongoose.Schema({
-    item_group_label: { type: String, required: true, unique: true }
-});
-
-const itemsGroupModel = mongoose.model('item-groups', itemsGroupSchema);
-
-module.exports = itemsGroupModel;
